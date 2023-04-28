f3d1f3c5f722431035c1cb9108db6c32a396d688
Nilet <nilet@tutanota.com>
2023-04-28 14:09:04 -0300

    testing env shit

--- src/pages/1.home/Home.tsx
+++ src/pages/1.home/Home.tsx
@@ -2,12 +2,14 @@ import LabsWrapper from "../../components/LabsWrapper";
 import '../../index.css'
 import Button from "../../components/Button";
 
+
 function Home() {
+    const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3001';
     return (
         <>
             <LabsWrapper />
             <Button content="testeHealthCheck" callback={async () => {
-                const response = await fetch("http://localhost:3001/health-check");
+                const response = await fetch(`${apiUrl}/health-check`);
                 const result = await response.json()
                 console.log(result)
             }} />
